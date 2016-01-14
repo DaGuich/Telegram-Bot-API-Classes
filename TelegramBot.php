219a1d0ceed0d67f2b97f8e31e54f416885161fe
Matthias Gilch <matthias.gilch.mg@gmail.com>
2016-01-14 21:25:25 +0100

    Added some functions
    First function getMe
    function call to handle the requests with curl
    function getcurlfile to get the curlFile in order to send the file

--- TelegramBot.php
+++ TelegramBot.php
@@ -19,12 +19,25 @@ class Telegram
     public $url;
 
     /**
-     * Name of the Telegram bot
-     * this comes from the getMe()-function
+     * Information about the running bot
      *
      * @var User
      */
     public $bot_information;
+
+    /**
+     * curl instance
+     *
+     * @var curl
+     */
+    protected $curl;
+
+    /**
+     * Array with supported attachments
+     *
+     * @var array
+     */
+    protected $attachments = array( 'audio' , 'document' , 'photo' , 'sticker' , 'video' );
 
     public function __construct( $api_token )
     {
@@ -35,7 +48,69 @@ class Telegram
 
         $this->api_token = $api_token;
         $this->url = 'https://api.telegram.org/bot'.$this->api_token.'/';
+        $this->curl = curl_init();
         
         $this->bot_information = $this->getMe();
+    }
+
+    public function getMe()
+    {
+        return new User( $this->call( 'getMe' ) );
+    }
+
+    public function call( $method , $data = NULL )
+    {
+        $options = array(
+            CURLOPT_URL => $this->url . $method,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => null,
+            CURLOPT_POSTFIELDS => null
+        );
+
+        if( !empty( $data ) )
+        {
+            foreach( $this->attachments as $attachment )
+            {
+                if( !empty( $data[$attachment] ) )
+                {
+                    $data[$attachment] = $this->getCurlFile( $data[$attachment] );
+                    break;
+                }
+            }
+
+            $options[CURLOPT_POST] = true;
+            $options[CURLOPT_POSTFIELDS] = $data;
+        }
+
+        curl_setopt_array( $this->curl , $options ); 
+
+        $response = curl_exec( $this->curl );
+        $response = json_decode( $response , true );
+
+        if( empty( $response ) OR $response["ok"] != true )
+        {
+            throw new Exception( $response['description'] , $response['error_code'] );
+
+            return $response['result'];
+        }
+    }
+
+    protected function getCurlFile( $file_path )
+    {
+        $real_path = realpath( $path );
+
+        if( empty( $real_path ) )
+        {
+            throw new Exception( 'File not found' );
+        }
+
+        if( !class_exists('CURLFile') )
+        {
+            throw new Exception( 'Please install php curl' );
+        }
+        else
+        {
+            return new CURLFile( $real_path );
+        }
     }
 }
